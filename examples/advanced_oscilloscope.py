8438497c91d8c3e2aaf9f06a20306dd91a65bee1
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-03-31 16:08:58 +0200

    Update advanced_oscilloscope.py

--- examples/advanced_oscilloscope.py
+++ examples/advanced_oscilloscope.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import numpy as np
+import json
 
 from deistools.acquisition import DEISchannel,ZPico5000a
 from pyeclab import BANDWIDTH, E_RANGE, I_RANGE, BiologicDevice, Channel, ChannelConfig, FileWriter
@@ -10,24 +11,32 @@ from trueformawg import import_awg_txt
 # ==== User parameters =====
 # Result saves
 saving_dir = 'E:/Experimental_data/Federico/2025/python_software_test'
-experiment_name = '2503181649_deischannel_ca_with_advancedpico'
+experiment_name = '2503311435_acquisition_ca_advancedpico_10_periods'
 # Measurment paramters
 capture_size = 300000000
 samples_total = 300000000
-sampling_time = 1
+sampling_time = 2
 sampling_time_scale = 'PS5000A_US'
 # STFFT-EIS parameters
-sample_size = int(1e6*100) # sampling frequency * analysis time
-frequencies = np.array([1000,100000])
+sample_size = int(2e6*100) # sampling frequency * analysis time
 irange = 1e-5
 # Downsampling parameters
 filter_order = 8
 cutoff = 90
-ds_factor = int(2e-3/1e-6) # new time step / original time step
+ds_factor = int(2e-3/2e-6) # new time step / original time step
 buffer_size = int(capture_size/ds_factor) # sampling time / acquisition time  (in s)
 # Potentiostat and EC-lab SDK
 ip_address = '172.28.26.10'
 binary_path = "C:/EC-Lab Development Package/EC-Lab Development Package/"
+# Define frequencies for online computation of impedance
+json_file = open('./2412111607multisine_splitted_100kHz-10mHz_8ptd_fgen1MHz_flat_norm_random_phases/high_freqs/waveform_metadata.json')
+waveform_metadata = json.load(json_file)
+multisine_freqs = np.array(waveform_metadata['Frequencies / Hz'])
+# Add a second multisine 
+json_file = open('./2412111607multisine_splitted_100kHz-10mHz_8ptd_fgen1MHz_flat_norm_random_phases/low_freqs/waveform_metadata.json')
+waveform_metadata = json.load(json_file)
+multisine_freqs = np.append(multisine_freqs,np.array(waveform_metadata['Frequencies / Hz']))
+frequencies = multisine_freqs[28:]
 # ===========================
 device = BiologicDevice(ip_address, binary_path = binary_path)
 config = ChannelConfig(
