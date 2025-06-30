62117ec1442c403b8d10c7aa3920f484c4e06af5
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-06-30 17:06:11 +0200

    Updated import

--- src/elma/deischannel.py
+++ src/elma/deischannel.py
@@ -6,9 +6,9 @@ from dataclasses import dataclass, field, asdict
 from typing import Union
 
 from pyeclab import Channel
-from deistools.acquisition.utils import ConditionAverage, check_software_limits, WaveFormSequence, condition_avarage_serialization_factory
-from deistools.acquisition.picocalculator import PicoCalculator
-from deistools.acquisition.multisinegen import MultisineGenerator, MultisineGeneratorCombined, custom_serialization_factory
+from deistools.acquisition.utils import ConditionAverage, check_software_limits, condition_avarage_serialization_factory
+from deistools.acquisition.picocalculator import PicoCalculator, check_software_limits_scope
+from deistools.acquisition.multisinegen import MultisineGenerator, MultisineGeneratorCombined, WaveFormSequence, custom_serialization_factory
 
 
 @dataclass
